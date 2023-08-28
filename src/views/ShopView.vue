72fd596ced09af3c505b2d480b8f2983c6786764
joshua-harmon <joshua.harmon1960@gmail.com>
2023-08-28 14:17:20 -0700

    Update ShopView.vue

--- src/views/ShopView.vue
+++ src/views/ShopView.vue
@@ -1,19 +1,3 @@
-<script>
-import {setPageTitle, setPageMetaData} from '@/seo';
-
-export default {
-  name: "ShopView",
-  setup() {
-    setPageTitle("Tienda - Vue Seo");
-    setPageMetaData([
-      {
-        name: "description",
-        content: "En la tienda tenemos cursos offline para que una vez los compres los puedas descargar para ver sin conexión"
-      }
-    ]);
-  }
-}
-</script>
 <template>
   <form
     name="ask-question"
