cc50fd8411b0bc17adf284dc281611f633a84732
joshua-harmon <joshua.harmon1960@gmail.com>
2023-08-28 15:26:41 -0700

    Update ShopView.vue

--- src/views/ShopView.vue
+++ src/views/ShopView.vue
@@ -1,2 +1,33 @@
 <template>
-  </template>
+
+  <netlify-form name='Contact' :form='form'>
+    <template #default='{readonly, submitting, submitted}'>
+
+      <p v-if='submitted'>Thanks for your submission</p>
+
+      <input
+        name='Email'
+        placeholder='Email'
+        v-model='form.Email'
+        :readonly='readonly'>
+
+      <button>
+        {{ submitting ? 'Submitting' : 'Submit' }}
+      </button>
+
+    </template>
+  </netlify-form>
+
+</template>
+
+<script>
+
+export default {
+  data: function() {
+    form: {
+      Email: ''
+    }
+  }
+}
+
+</script>
