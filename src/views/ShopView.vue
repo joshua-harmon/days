be2104b0b78fd7f9efe5b41c546a37da35043656
joshua-harmon <joshua.harmon1960@gmail.com>
2023-08-28 14:25:32 -0700

    Update ShopView.vue

--- src/views/ShopView.vue
+++ src/views/ShopView.vue
@@ -1,38 +1,18 @@
 <template>
-  <form
-    name="ask-question"
-    method="post"
-    data-netlify="true"
-    data-netlify-honeypot="bot-field"
-    >
-    <input type="hidden" name="form-name" value="ask-question" />
-    <label v-for="(panelist, index) in panelists" :key="index">
-      <input
-        type="radio"
-        name="panelist"
-        :value="panelist"
-        @input="ev => updatePanelist"
-        :checked="panelist === currentPanelist"
-      />
-      <span>{{ panelist }}</span>
+<form name="fileForm" enctype="multipart/form-data" data-netlify="true">
+  <p>
+    <label>
+      <span>Name:</span>
+      <input name="name" type="text"/>
     </label>
-    ...
-    <button>Submit</button>
-  </form>
+  </p>
+  <p>
+    <label>
+      <span>Add file:</span>
+      <input name="file" type="file"/>
+    </label>
+  </p>
+  <button>Submit</button>
+</form>
+<p class="result"></p>
 </template>
-<script>
-export default {
-  name: "QAForm",
-  methods: {
-    updatePanelist (ev) {
-      this.currentPanelist = ev.target.value
-    }
-  },
-  data () {
-    return {
-      panelists: ['Evan You', 'Chris Fritz'],
-      currentPanelist: 'Evan You'
-    }
-  }
-}
-</script>
