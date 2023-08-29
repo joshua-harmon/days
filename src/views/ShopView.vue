2b4772e421e17a98671d9b8d5a04fa942633dfef
joshua-harmon <joshua.harmon1960@gmail.com>
2023-08-28 18:45:53 -0700

    Update ShopView.vue

--- src/views/ShopView.vue
+++ src/views/ShopView.vue
@@ -1,4 +1,8 @@
 
     <template>
-        
+        <form action="https://formsubmit.co/joshua.harmon1960@gmail.com" method="POST">
+     <input type="text" name="name" required>
+     <input type="email" name="email" required>
+     <button type="submit">Send</button>
+</form>
     </template>
