2c3ff173f8b2ed486c7845d264e4030419e1d8fe
joshua-harmon <joshua.harmon1960@gmail.com>
2023-08-28 18:48:56 -0700

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -43,7 +43,12 @@ export default {
   <p>
     <button type="submit">Send</button>
     </p>
+</form><form action="https://formsubmit.co/joshua.harmon1960@gmail.com" method="POST">
+     <input type="text" name="name" required>
+     <input type="email" name="email" required>
+     <button type="submit">Send</button>
 </form>
+    
   </div>
 </body>
 </template>
