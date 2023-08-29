5cd681b43157c21f83029d1fa0ae57c836636afc
joshua-harmon <joshua.harmon1960@gmail.com>
2023-08-28 20:19:28 -0700

    Update ContactView.vue

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -18,4 +18,5 @@ export default {
 <template>
 
   <li><a href="https://preeminent-selkie-6b43a6.netlify.app">menu</a></li>
+  <li><a href="https://form.jotform.com/232398357846168">appn</a></li>
 </template>
